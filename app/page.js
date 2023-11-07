05cc9126764d2bfbc49ecebf98d780e07a619f49
adlsdztony <zzl0712@outlook.com>
2023-11-08 01:08:13 +0800

    Refactor WikiPage component and add argument handling to Home component

--- app/page.js
+++ app/page.js
@@ -1,69 +1,20 @@
 "use client";
 
-import Markdown from "react-markdown";
-import { useEffect, useState } from "react";
-import MarkNav from "markdown-navbar";
-import "markdown-navbar/dist/navbar.css";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
-
-
-function makePublicUrl(path) {
-  const githubRow = "https://raw.githubusercontent.com/EZ-HKU/Wiki/main/public";
-  return githubRow + path;
-}
-
+import React from "react";
+import WikiPage from "@/components/WikiPage";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
+  // get argument from url
+  const searchParams = useSearchParams();
+  const page = searchParams.get("page");
+  if (page) {
+    return <WikiPage fileName={page + ".md"} />;
+  }
 
-  // load markdown from public/README.md
-  const [markdown, setMarkdown] = useState("");
-  useEffect(() => {
-    fetch(makePublicUrl("/UniScript.md"))
-      .then((response) => response.text())
-      .then((text) => setMarkdown(text));
-  }, []);
   return (
-    <div>
-      <div className="nav-container">
-        <MarkNav
-          className="article-menu"
-          source={markdown}
-          headingTopOffset={0}
-          ordered={false}
-        />
-      </div>
-      <div className="article-container rounded-3xl">
-        <Markdown
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || "");
-              return !inline && match ? (
-                <SyntaxHighlighter
-                  showLineNumbers={true}
-                  style={prism}
-                  language={match[1]}
-                  PreTag="div"
-                  {...props}
-                >
-                  {String(children).replace(/\n$/, "")}
-                </SyntaxHighlighter>
-              ) : (
-                // else return inline code
-                className = "font-mono bg-gray-300 rounded-md p-0.5",
-                <a className={className} {...props}>
-                  {children}
-                </a>
-              );
-            }
-          }}
-          // component for link
-    
-        >
-          {markdown}
-        </Markdown>
-      </div>
-    </div>
+    <h1 className="text-4xl font-bold text-center">
+      Home Page
+    </h1>
   );
 }
---
+++ components/WikiPage.js
@@ -0,0 +1,69 @@
+"use client";
+
+import Markdown from "react-markdown";
+import { useEffect, useState } from "react";
+import MarkNav from "markdown-navbar";
+import "markdown-navbar/dist/navbar.css";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+
+
+function makePublicUrl(path) {
+  const githubRow = "https://raw.githubusercontent.com/EZ-HKU/Wiki/main/public/";
+  return githubRow + path;
+}
+
+
+export default function WikiPage({ fileName }) {
+
+  // load markdown from public/README.md
+  const [markdown, setMarkdown] = useState("");
+  useEffect(() => {
+    fetch(makePublicUrl(fileName))
+      .then((response) => response.text())
+      .then((text) => setMarkdown(text));
+  }, [fileName]);
+  return (
+    <div>
+      <div className="nav-container">
+        <MarkNav
+          className="article-menu"
+          source={markdown}
+          headingTopOffset={0}
+          ordered={false}
+        />
+      </div>
+      <div className="article-container rounded-3xl">
+        <Markdown
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return !inline && match ? (
+                <SyntaxHighlighter
+                  showLineNumbers={true}
+                  style={prism}
+                  language={match[1]}
+                  PreTag="div"
+                  {...props}
+                >
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              ) : (
+                // else return inline code
+                className = "font-mono bg-gray-300 rounded-md p-0.5",
+                <a className={className} {...props}>
+                  {children}
+                </a>
+              );
+            }
+          }}
+          // component for link
+    
+        >
+          {markdown}
+        </Markdown>
+      </div>
+    </div>
+  );
+}
